0c15a8d114fddc15227b153de2918a15c2f3cb64
Tab10id <tabloidmeister@gmail.com>
2020-10-28 23:40:37 +0300

    fix code style

--- main.rb
+++ main.rb
@@ -3,26 +3,24 @@
 require 'rack'
 require 'faye/websocket'
 
-app = lambda do |env|
-  if Faye::WebSocket.websocket?(env)
-    ws = Faye::WebSocket.new(env)
+app =
+  lambda do |env|
+    if Faye::WebSocket.websocket?(env)
+      ws = Faye::WebSocket.new(env)
 
-    ws.on :message do |event|
-      ws.send(event.data)
+      ws.on :message do |event|
+        ws.send(event.data)
+      end
+
+      ws.on :close do |_event|
+        ws = nil
+      end
+
+      ws.rack_response
+
+    else
+      [200, { 'Content-Type' => 'text/plain' }, ['Hello']]
     end
-
-    ws.on :close do |event|
-      p [:close, event.code, event.reason]
-      ws = nil
-    end
-
-    # Return async Rack response
-    ws.rack_response
-
-  else
-    # Normal HTTP request
-    [200, { 'Content-Type' => 'text/plain' }, ['Hello']]
   end
-end
 
 Rack::Server.start(app: Rack::ShowExceptions.new(app), Port: 9292)
